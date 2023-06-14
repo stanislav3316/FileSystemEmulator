9f0bd06ab73dadf37e0fb8c806900d95bc2de94a
Stanislav Bolsun <bolsun@arrival.com>
2023-06-14 18:00:29 +0200

    re-declare FileSystem

--- src/main/kotlin/com/jb/FileSystem.kt
+++ src/main/kotlin/com/jb/FileSystem.kt
@@ -2,29 +2,38 @@ package com.jb
 
 import java.io.File
 
+//todo: declare domain exceptions
 interface FileSystem {
 
-    //todo: declare domain exceptions
-    //todo: should we implement `edit` operations?
+    fun save(file: File): Unit
 
-    fun create(file: File): Unit
+    fun save(path: FsPath, bytes: ByteArray): Unit
 
-    fun read(path: Path): Array<Byte>
+    fun append(path: FsPath, bytes: Array<Byte>): Unit
 
-    fun append(path: Path, content: Array<Byte>): Unit
+    fun delete(path: FsPath): Unit
 
-    fun delete(path: Path): Unit
+    fun rename(path: FsPath, newName: FsFileName): Unit
 
-    fun rename(path: Path, newName: FileName): Unit
+    fun move(path: FsPath, newPath: FsPath): Unit
 
-    fun move(path: Path, newPath: Path): Unit
+    fun read(path: FsPath): Array<Byte>
+
+    fun ls(path: FsPath): List<FsEntity>
 
     companion object {
 
         @JvmInline
-        value class Path(val value: String)
+        value class FsPath(val value: String)
 
         @JvmInline
-        value class FileName(val value: String)
+        value class FsFileName(val value: String)
+
+        data class FsEntity(
+            val name: String,
+            val fullPath: String,
+            val isDirectory: Boolean,
+            val size: Long
+        )
     }
 }
--- src/main/kotlin/com/jb/InFileFS.kt
+++ src/main/kotlin/com/jb/InFileFS.kt
@@ -1,32 +1,41 @@
 package com.jb
 
-import com.jb.FileSystem.Companion.FileName
-import com.jb.FileSystem.Companion.Path
+import com.jb.FileSystem.Companion.FsEntity
+import com.jb.FileSystem.Companion.FsFileName
+import com.jb.FileSystem.Companion.FsPath
 import java.io.File
 
-class InFileFS(val fs: Path) : FileSystem {
+class InFileFS(val fs: FsPath) : FileSystem {
 
-    override fun create(file: File) {
+    override fun save(file: File) {
         TODO("Not yet implemented")
     }
 
-    override fun read(path: Path): Array<Byte> {
+    override fun save(path: FsPath, bytes: ByteArray) {
         TODO("Not yet implemented")
     }
 
-    override fun append(path: Path, content: Array<Byte>) {
+    override fun append(path: FsPath, bytes: Array<Byte>) {
         TODO("Not yet implemented")
     }
 
-    override fun delete(path: Path) {
+    override fun delete(path: FsPath) {
         TODO("Not yet implemented")
     }
 
-    override fun rename(path: Path, newName: FileName) {
+    override fun rename(path: FsPath, newName: FsFileName) {
         TODO("Not yet implemented")
     }
 
-    override fun move(path: Path, newPath: Path) {
+    override fun move(path: FsPath, newPath: FsPath) {
+        TODO("Not yet implemented")
+    }
+
+    override fun read(path: FsPath): Array<Byte> {
+        TODO("Not yet implemented")
+    }
+
+    override fun ls(path: FsPath): List<FsEntity> {
         TODO("Not yet implemented")
     }
 }
